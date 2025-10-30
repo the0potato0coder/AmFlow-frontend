8c9e8084450393b8e51e47d3871d5ce0baa8a40e
the0potato0coder <jugantarroy27@gmail.com>
2025-10-30 15:46:57 +0530

    modified:   src/api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.VITE_API_URL,
   // You can add default headers here if needed
 });
 
